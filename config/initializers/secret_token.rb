8715eb1788cf1937ad65afe52c162e9a1cc3bc53
Gareth G. Aye <gareth.aye@gmail.com>
2012-04-07 10:37:31 -0400

    Adds rails web app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rideshare::Application.config.secret_token = 'aec1ef0923f39557c479a35569c9761dbdc9bf6eea66c61e58c6b040c24fd0a66080ffd13ac7bd07227d9b3cc88eb613abb64d90b4f7c54de237670fbd243666'
