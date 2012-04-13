8c2da4ea61547ddebd73fef9a6e62ef9c8f93dd2
Brian Ayers <bayers@middlebury.edu>
2012-04-13 11:30:59 -0400

    Preliminary unit tests

--- test/unit/trip_test.rb
+++ test/unit/trip_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TripTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test "trip attributes must not be empty" do
+	trip = Trip.new
+	assert trip.invalid?
+	assert trip.errors[:date].any?
+	assert trip.errors[:time].any?	# one of the time ranges we set up
+	assert trip.errors[:location_start].any?
+	assert trip.errors[:location_finish].any?
+	assert trip.errors[:driver].any?  # should always be the posting user..
+	assert trip.errors[:seats_available].any?
+	assert trip.errors[:cost_trip].any? # gas, mileage, etc. calculated by us
+	assert trip.errors[:cost_seat].any? # additional fee set by user (can be $0)
+ end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user attributes must not be empty" do
+	user = User.new
+	assert user.invalid?
+	assert user.errors[:name_first].any?
+	assert user.errors[:name_last].any?
+	assert user.errors[:email].any? # check it is middlebury.edu in validator
+	assert user.errors[:password].any?
+	# sign in using facebook?
+ end
 end
