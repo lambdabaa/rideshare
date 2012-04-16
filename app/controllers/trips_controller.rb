0e0e39cad656a94f72a1383d9e502e03f53beadd
Gareth G. Aye <gareth.aye@gmail.com>
2012-04-16 18:53:19 -0400

    Ruby-ifies the links in the nav bar, adds actions and routes for basic index and show views on trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,13 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+    # TODO(gaye): Replace this with an actual view
+    render :text => @trips.to_json
+  end
+  
+  def show
+    @trip = Trip.find(params[:id])
+    # TODO(gaye): Replace this with an actual view
+    render :text => @trip.to_json
+  end
 end
