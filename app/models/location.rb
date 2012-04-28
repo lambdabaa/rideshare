ab767c6a733040e9d5a5ddedadb2a5c1f1996b12
gaye <gareth.aye@gmail.com>
2012-04-28 11:24:26 -0400

    Adds first version of search via filter function on Trip model

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,10 @@ class Location < ActiveRecord::Base
   has_many :trips_finishing_at, :foreign_key => "finish_location_id", :class_name => "Trip"
   
   # TODO(gaye): Add validations
+  
+  def self.distance(a, b)
+    Math.sqrt(
+        ((a.latitude - b.latitude) ** 2) +
+        ((a.longitude - b.longitude) ** 2))
+  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,4 +6,23 @@ class Trip < ActiveRecord::Base
   belongs_to  :finish_location, :primary_key => "finish_location_id", :class_name => "Location"
 
   # TODO(gaye): Add validations
+  
+  def filter_by_date_and_start_and_finish_location(
+    first_date,
+    last_date,
+    start_location,
+    finish_location,
+    max_start_distance_offset=0.5,
+    max_finish_distance_offset=0.5)
+    
+    # TODO(gaye): We should be storing these redundantly in the db 
+    # so we don't have to do these linear time filterings
+    trips = Trip.where("departure between #{first_date} and #{last_date}")
+    trips.delete_if {|trip| 
+        Location.distance(start_location, trip.start_location) > max_start_distance_offset}
+    trips.delete_if {|trip|
+        Location.distance(finish_location, trip.finish_location) > max_finish_distance_offset}
+    
+    # TODO(gaye): Maybe sort by proximity on the filtered list
+  end
 end
