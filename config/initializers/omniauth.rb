3d603a14a22d3f74db2f229e12f2b5fba170d2b4
brian <bayers@middlebury.edu>
2012-04-27 21:24:34 -0400

    changed inlines

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '224061857645056', '95f60e2e738851b66e0046cda25e8022',
-  		   :scope => 'email', :display => 'popup'
+      :scope => 'email', :display => 'popup'
 end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rideshare::Application.routes.draw do
   root :to => 'home#index'
   
-  match "/auth/:provider/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
+   match "/auth/:provider/callback" => "sessions#create"
+   match "/signout" => "sessions#destroy", :as => :signout
   
   resources :trips
 end
