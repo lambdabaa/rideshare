811ab78aed00e297cf37d36fbfda66e5ee757e5a
Armaan Sarkar <armaansarkar@gmail.com>
2012-05-02 16:11:09 -0400

    autocomplete proxy only processes requests to google host

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,18 +3,21 @@ require "uri"
 
 class LocationsController < ApplicationController
   def autocomplete_proxy
+    puts "requested:"
+    puts request.domain
     uri = URI.parse(CGI.unescape(params["url"]))
     http = Net::HTTP.new(uri.host, uri.port)
+    if uri.host == "maps.googleapis.com"
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(uri.request_uri)
+      body = http.request(request).body
     
-    request = Net::HTTP::Get.new(uri.request_uri)
-    body = http.request(request).body
-    
-    respond_to do |format|
-      format.html
-      format.json { render :json => body }
+      respond_to do |format|
+        format.html
+        format.json { render :json => body }
+      end
     end
   end
 end
