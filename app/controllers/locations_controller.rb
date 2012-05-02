9e3d94d816f3df7d166744991a3c2e25f11f9b37
Armaan Sarkar <armaansarkar@gmail.com>
2012-05-02 15:51:52 -0400

    fixes cross-domain autocomplete issue

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,20 @@
+require "net/https"
+require "uri"
+
 class LocationsController < ApplicationController
+  def autocomplete_proxy
+    uri = URI.parse(CGI.unescape(params["url"]))
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    request = Net::HTTP::Get.new(uri.request_uri)
+    body = http.request(request).body
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => body }
+    end
+  end
 end
