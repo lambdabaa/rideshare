e3a83fb809b94950b011409d786c4e6986619ae0
Daniel Trauner <danielhtrauner@gmail.com>
2012-05-10 13:38:37 -0400

    Added a missing comma

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '224061857645056', '95f60e2e738851b66e0046cda25e8022'
+  provider :facebook, '224061857645056', '95f60e2e738851b66e0046cda25e8022',
     # Commenting out the line below breaks FB authentication on Daisy's Windows machine.  Why?
     :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
