92e9b59124ef6b02fcb6fa4183be119a4940e5c8
Daniel Trauner <danielhtrauner@gmail.com>
2012-05-10 02:31:10 -0400

    I have done your bidding, my master

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '224061857645056', '95f60e2e738851b66e0046cda25e8022',
-    :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
+    # Uncommenting the line below gets FB authentication working on Daisy's Windows machine.  Why?
+    #:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
 
   
