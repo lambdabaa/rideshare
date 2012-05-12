30aceff6cc38e899dbbbc6150a34a7da8e822881
brian <bayers@middlebury.edu>
2012-05-12 16:08:08 -0400

    wrote unit tests for users

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should create a new user if one doesn't already exist" do
+    auth = { "provider" => 'facebook', "uid" => '122', "info" => {"name" => 'Brian Ayers', "urls" => {"Facebook" => 'facebook_link'}, "image" =>'image_link' }}
+    
+    b = users(:brian)
+    #make sure it doesn't already exist
+    database_entry = User.find_by_uid(b.uid)
+    database_entry.destroy if (database_entry)
+    
+    User.find_by_provider_and_uid(b.provider, b.uid)
+    new_entry = User.create_with_omniauth(auth)
+    assert_equal(new_entry.name, b.name)
+    assert_equal(new_entry.provider, b.provider)
+    assert_equal(new_entry.uid, b.uid)
+    assert_equal(new_entry.url, b.url)
+    assert_equal(new_entry.image, b.image)
+  end
 end
