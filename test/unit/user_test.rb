836f1b95aa485bda0494ffbdd57619e081220d91
brian <bayers@middlebury.edu>
2012-05-12 19:46:50 -0400

    updated user unit tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,19 +3,18 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   test "should create a new user if one doesn't already exist" do
-    auth = { "provider" => 'facebook', "uid" => '122', "info" => {"name" => 'Brian Ayers', "urls" => {"Facebook" => 'facebook_link'}, "image" =>'image_link' }}
+    auth = { "provider" => 'facebook', "uid" => '122', "info" => {"name" => 'Tommy Jones', "urls" => {"Facebook" => 'facebook_link'}, "image" =>'image_link' }}
     
-    b = users(:brian)
+    tommy=users(:tommy)
     #make sure it doesn't already exist
-    database_entry = User.find_by_uid(b.uid)
+    database_entry = User.find_by_uid(tommy.uid)
     database_entry.destroy if (database_entry)
     
-    User.find_by_provider_and_uid(b.provider, b.uid)
     new_entry = User.create_with_omniauth(auth)
-    assert_equal(new_entry.name, b.name)
-    assert_equal(new_entry.provider, b.provider)
-    assert_equal(new_entry.uid, b.uid)
-    assert_equal(new_entry.url, b.url)
-    assert_equal(new_entry.image, b.image)
+    assert_equal(new_entry.name, tommy.name)
+    assert_equal(new_entry.provider, tommy.provider)
+    assert_equal(new_entry.uid, tommy.uid)
+    assert_equal(new_entry.url, tommy.url)
+    assert_equal(new_entry.image, tommy.image)
   end
 end
