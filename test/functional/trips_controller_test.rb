b3eee8da305d6ca7498a4d5e940d95a48f78910e
Gareth G. Aye <gareth.aye@gmail.com>
2012-05-15 13:27:41 -0400

    Adds test for trip filter

--- test/functional/trips_controller_test.rb
+++ test/functional/trips_controller_test.rb
@@ -1,11 +1,15 @@
 require 'test_helper'
 
 class TripsControllerTest < ActionController::TestCase
+  # TODO(gaye): Uncomment this once the facebook code is unbroken
   # test "index without params gives success" do
   #   get :index
   #   assert_response :success
   # end
   
+  # TODO(gaye): Test search
+  
+  # TODO(gaye): Uncomment this once the facebook code is unbroken
   # test "show gives success" do
   #   trip = trips(:middlebury_to_boston)
   #   get :show, :id => trip.id
--- test/unit/trip_test.rb
+++ test/unit/trip_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class TripTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  
-  # TODO(gaye): Test filter
+  test "filter works appropriately" do
+    trip1 = trips(:middlebury_to_boston)
+    trip2 = trips(:boston_to_middlebury)
+    start = locations(:middlebury)
+    finish = locations(:boston)
+    trips = Trip.filter_by_date_and_start_and_finish_location(
+      trip1.departure - 60 * 60 * 24,
+      trip1.departure + 60 * 60 * 24,
+      trip1.start_location,
+      trip1.finish_location)
+    assert(trips.include?(trip1))
+    assert(!trips.include?(trip2))
+  end
 end
