88eb6da409b610782e2a4bce31bd39cd8180cd18
Gareth G. Aye <gareth.aye@gmail.com>
2012-05-15 13:36:06 -0400

    Styling changes

--- app/models/ride.rb
+++
@@ -1,9 +0,0 @@
-# Rides connect the User and Trip models in a many-to-many relationship
-# Passengers can take many trips and trips can have many passengers
-class Ride < ActiveRecord::Base
-  belongs_to :trip
-  belongs_to :passenger, :primary_key => "passenger_id", :foreign_key => "user_id", :class_name => "User"
-
-  validates :trip_id, :numericality => true
-  validates :passenger_id, :numericality => true
-end
