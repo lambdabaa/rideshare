e61faa11c5931ae7caf67cb91a6c2c97430d8863
brian <bayers@middlebury.edu>
2012-05-15 11:24:01 -0400

    small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,21 +13,11 @@ class User < ActiveRecord::Base
   
   def self.update_with_omniauth(auth)
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
-    if user.provider != auth["provider"]
-      user.provider = auth["provider"]
-    end
-    if user.uid != auth["uid"]
-      user.uid = auth["uid"]
-    end
-    if user.name != auth["info"]["name"]
-      user.name = auth["info"]["name"]
-    end
-    if user.url != auth["info"]["urls"]["Facebook"]
-      user.url = auth["info"]["urls"]["Facebook"]
-    end
-    if user.image != auth["info"]["image"]
-      user.image = auth["info"]["image"]
-    end
-  return user
+    user.provider = auth["provider"]
+    user.uid = auth["uid"]
+    user.name = auth["info"]["name"]
+    user.url = auth["info"]["urls"]["Facebook"]
+    user.image = auth["info"]["image"]
+    return user
   end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @auth = { "provider" => 'facebook', "uid" => '122', "info" => {"name" => 'Tommy Jones', "urls" => {"Facebook" => 'facebook_link'}, "image" =>'image_link' }}
     @tommy=users(:tommy)
   end
   
@@ -11,9 +10,10 @@ class UserTest < ActiveSupport::TestCase
     #Delete tommy
     database_entry = User.find_by_uid(@tommy.uid)
     database_entry.destroy
-    assert_nil(User.find_by_provider_and_uid(@auth["provider"], @auth["uid"]))
+    assert_nil(User.find_by_provider_and_uid(@tommy["provider"], @tommy["uid"]))
     #Create tommy 
-    new_entry = User.create_with_omniauth(@auth)
+    auth = { "provider" => 'facebook', "uid" => '122', "info" => {"name" => 'Tommy Jones', "urls" => {"Facebook" => 'facebook_link'}, "image" =>'image_link' }}
+    new_entry = User.create_with_omniauth(auth)
     assert_equal(new_entry.name, @tommy.name)
     assert_equal(new_entry.provider, @tommy.provider)
     assert_equal(new_entry.uid, @tommy.uid)
